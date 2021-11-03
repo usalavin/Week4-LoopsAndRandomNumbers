7c5dba55a42d54046be6c1a084d83f037b485fde
unknown <Uljana alaviba>
2021-11-03 14:15:35 +0200

    add new projects

---
+++ GuessingGameInfinite/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GuessingGameInfinite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit eri arvama;
+            //kui kasutaja suutis seda numbrit  ära arvata, siis ta on mängu võitnud;
+            //katsete arv on piiramatu.
+            //*programm genereerib juhuslikku numbrit vaid ühte korda.
+
+            Random rnd = new Random();
+            int cpNumber = rnd.Next(1, 11);
+            bool a = true;
+           
+            
+
+            while (a)
+            {
+                Console.WriteLine($"Sisesta number 1-10");
+                int myRandomNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (myRandomNumber == cpNumber)
+                {
+                    Console.WriteLine("Oled võitja!");
+                    a = false;
+                }
+            }
+
+
+
+            Console.WriteLine("Kena päeva!");
+            
+        }
+    }
+    }
+
---
+++ GuessingGameThreeTries/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GuessingGameThreeTries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit eri arvama;
+            //kui kasutaja suutis seda numbrit  ära arvata, siis ta on mängu võitnud;
+            //kasutajal on kolm katset, kui kasutaja ei ole suutnud kolme katsega numbrit ära arvata, siis mängu võidab arvuti.
+            //*programm genereerib juhuslikku numbrit vaid ühte korda.
+
+            Random rnd = new Random();
+            int cpNumber = rnd.Next(1, 11);
+            int i = 0;
+            while (i < 3)
+            {
+                Console.WriteLine("Sisesta number 1-10");
+
+                int myRandomNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (myRandomNumber == cpNumber)
+                {
+                    Console.WriteLine("Olete võitja!");
+                    break;
+                }
+
+                else
+                {
+                    i++;
+                    Console.WriteLine($"Vale. {3 - i} katset on jäänud.");
+                }
+
+                Console.WriteLine("Arvuti võit!");
+
+            } 
+
+        }
+    }
+}
